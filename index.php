308d8334627f9db908d9e0855aaf8c136d25add1
SergioGenesis97 <sergio.bautista@alumnos.udg.mx>
2021-10-02 13:04:48 -0500

    modificacion del template

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php include './resources/layouts/header.php' ?>
 
+
+    <!-- card -->
+    <div class="card  card-dark">
+        <div class="card-header">
+            <h3 class="card-title">Default Card Example</h3>
+            <div class="card-tools">
+                <!-- Buttons, labels, and many other things can be placed here! -->
+                <!-- Here is a label for example -->
+                <span class="badge badge-primary">Label</span>
+            </div>
+            <!-- /.card-tools -->
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+            The body of the card
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+            The footer of the card
+        </div>
+        <!-- /.card-footer -->
+    </div>
+    <!-- /.card -->
+
         <!-- Contenido -->
 
 
--- resources/layouts/header.php
+++ resources/layouts/header.php
@@ -52,3 +52,4 @@
             <!-- /.content-header -->
             <section class="content">
                 <div class="container-fluid">
+                    
